9f07360729ba4ad1b14b03ade29c1f0c6621d47d
santiagog048 <gallo9290@gmail.com>
2024-02-24 15:56:32 -0500

    Ejercicio

---
+++ Taller_1/Ejercicio7_Recursivos.py
@@ -0,0 +1,9 @@
+#Ejercicio 7 (Recursivo)
+
+def suma_digitos(numero):
+    if numero < 10:
+        return numero
+    else:
+        ultimo_digito = numero % 10
+        resto_numero = numero // 10
+        return ultimo_digito + suma_digitos(resto_numero)
