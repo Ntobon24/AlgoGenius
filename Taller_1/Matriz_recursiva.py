3f9f0d05792e7fad93ce86047eb22fe13bddfc19
Nicolas Tobon <nicolastobon242>
2024-02-24 15:09:58 -0500

    Recorrer la columna derecha de abajo hacia arriba

--- Taller_1/Matriz_recursiva.py
+++ Taller_1/Matriz_recursiva.py
@@ -9,3 +9,7 @@ def recorrer_matriz(matriz, inicio_fila, fin_fila, inicio_columna, fin_columna):
 
     for i in range(inicio_columna + 1, fin_columna):
         elementos.append(matriz[fin_fila - 1][i])
+
+    if inicio_fila != fin_fila - 1:
+            for i in range(fin_fila - 2, inicio_fila - 1, -1):
+                elementos.append(matriz[i][fin_columna - 1])
