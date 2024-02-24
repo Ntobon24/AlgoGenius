8196b21a2a6664c25de46253cec9e041953a0482
santiagog048 <gallo9290@gmail.com>
2024-02-24 15:46:15 -0500

    Ejemplo de Uso Funcional

--- Taller_1/Ejercicio1_NoRecursivos.py
+++ Taller_1/Ejercicio1_NoRecursivos.py
@@ -5,3 +5,7 @@ def sumar_array(array):
     for elemento in array:
         suma += elemento
     return suma
+
+mi_arreglo = [1, 2, 3, 4, 5]
+resultado = sumar_array(mi_arreglo)
+print("La suma de los elementos del arreglo es:", resultado)
