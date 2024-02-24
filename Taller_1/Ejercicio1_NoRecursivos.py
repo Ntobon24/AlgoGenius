f2479ea311d887b8a52da987b22f396544d112b6
santiagog048 <gallo9290@gmail.com>
2024-02-24 15:47:51 -0500

    Complejidad Temporal

--- Taller_1/Ejercicio1_NoRecursivos.py
+++ Taller_1/Ejercicio1_NoRecursivos.py
@@ -1,11 +1,9 @@
 #Ejercicio 1 (No Recursivo)
 
-def sumar_array(array):
-    suma = 0
-    for elemento in array:
-        suma += elemento
-    return suma
+def sumar_array(array): # O(n)
+    suma = 0 # O(0)
+    for elemento in array: # O(n)
+        suma += elemento # O(n)
+    return suma # O(0)
 
-mi_arreglo = [1, 2, 3, 4, 5]
-resultado = sumar_array(mi_arreglo)
-print("La suma de los elementos del arreglo es:", resultado)
+#Complejidad Temporal: O(n)
