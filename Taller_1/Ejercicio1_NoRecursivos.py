20bef94487959f92f47bdbc50be28f695801db0c
santiagog048 <gallo9290@gmail.com>
2024-02-24 15:49:42 -0500

    Complejidad Temporal - Espacial

--- Taller_1/Ejercicio1_NoRecursivos.py
+++ Taller_1/Ejercicio1_NoRecursivos.py
@@ -1,9 +1,10 @@
 #Ejercicio 1 (No Recursivo)
 
-def sumar_array(array): # O(n)
-    suma = 0 # O(0)
-    for elemento in array: # O(n)
-        suma += elemento # O(n)
-    return suma # O(0)
+def sumar_array(array): # O(n), O(1)
+    suma = 0 # O(0), O(1)
+    for elemento in array: # O(n), O(1)
+        suma += elemento # O(n), O(1)
+    return suma # O(0), O(1)
 
 #Complejidad Temporal: O(n)
+#Complejidad Espacial: O(1)
