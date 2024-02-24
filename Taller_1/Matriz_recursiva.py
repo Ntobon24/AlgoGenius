d93b6d806588705c8b343b0f3dbe4024e4291ed7
Nicolas Tobon <nicolastobon242>
2024-02-24 15:09:10 -0500

    Recorrer la fila inferior de izquierda a derecha

--- Taller_1/Matriz_recursiva.py
+++ Taller_1/Matriz_recursiva.py
@@ -6,3 +6,6 @@ def recorrer_matriz(matriz, inicio_fila, fin_fila, inicio_columna, fin_columna):
 
     for i in range(inicio_fila, fin_fila):
         elementos.append(matriz[i][inicio_columna])
+
+    for i in range(inicio_columna + 1, fin_columna):
+        elementos.append(matriz[fin_fila - 1][i])
