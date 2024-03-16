0419f8b4f8b79ac31904db5654496faca237d042
Matt <116335873+TartarusBoss@users.noreply.github.com>
2024-03-16 13:51:21 -0500

    Update ViewExercise.py
    Segundo commit en el cuál se terminó la lógica para determinar la fórmula a utilizar

--- Taller_2/View/ViewExercise.py
+++ Taller_2/View/ViewExercise.py
@@ -26,3 +26,35 @@ class ViewExercise:
         continuar = input().lower() == 's'
         if not continuar:
             break
+
+        
+    def determinar_tecnica(self, orden_importa, elementos_se_repiten, todos_elementos_seleccionados):
+        if todos_elementos_seleccionados:
+            if orden_importa:
+                if elementos_se_repiten:
+                    return "Permutaciones con repetición"
+                else:
+                    return "Permutaciones sin repetición"    
+                
+            else:
+                return "El orden sí importa para las permutaciones"    
+            
+        else: 
+            if orden_importa:
+                if elementos_se_repiten:
+                    return "Variaciones con repeticion"
+                else:
+                    return "Variaciones sin repeticion"     
+                
+            else:
+                if elementos_se_repiten:
+                    return "Combinaciones con repeticion"
+                else: 
+                    return "Combinaciones sin repeticion"
+                
+
+if __name__ == "__main__":
+    vista = ViewExercise()
+    vista.mostrar_menu()
+
+            
