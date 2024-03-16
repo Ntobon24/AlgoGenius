c31d89aa4e6b22a3a9eb133255e89ef3f346ee14
santiagog048 <gallo9290@gmail.com>
2024-03-16 16:07:02 -0500

    Menu Secundario 1

--- Taller_2/View/ViewExplication.py
+++ Taller_2/View/ViewExplication.py
@@ -5,3 +5,9 @@ def mostrar_menu_principal():
     print("2. Principios básicos de conteo")
     print("3. Permutaciones, Combinaciones y Variaciones")
     print("4. Salir")
+def mostrar_menu_conteo():
+    print("\nPrincipios básicos de conteo:")
+    print("Seleccione una opción: --> ")
+    print("\n1. Principio de la suma")
+    print("2. Principio del producto")
+    print("3. Volver al menú principal")
