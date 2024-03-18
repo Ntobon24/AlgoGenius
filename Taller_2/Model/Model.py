6831b8861cd9a69d2902faf82178ce6cbfa5e8b5
Matt <116335873+TartarusBoss@users.noreply.github.com>
2024-03-17 23:39:28 -0500

    Update Model.py

--- Taller_2/Model/Model.py
+++ Taller_2/Model/Model.py
@@ -4,25 +4,25 @@ import math
 class Formulas:
 
     @staticmethod
-    def permutaciones_sin_repeticion(n, r):
+    def permutaciones_sin_repeticion(n, r):    #Complejidad temporal: O(n) , Complejidad espacial: O(1)
         return math.factorial(n) // math.factorial(n - r)
 
     @staticmethod
-    def permutaciones_con_repeticion(n, r):
+    def permutaciones_con_repeticion(n, r):    #Complejidad temporal: O(1) , Complejidad espacial: O(1)
         return n ** r
 
     @staticmethod
-    def variaciones_sin_repeticion(n, r):
+    def variaciones_sin_repeticion(n, r):    #Complejidad temporal: O(n) , Complejidad espacial: O(1)
         return math.factorial(n) // math.factorial(n - r)
 
     @staticmethod
-    def variaciones_con_repeticion(n, r):
+    def variaciones_con_repeticion(n, r):    #Complejidad temporal: O(1) , Complejidad espacial: O(1)
         return n ** r
 
     @staticmethod
-    def combinaciones_sin_repeticion(n, r):
+    def combinaciones_sin_repeticion(n, r):    #Complejidad temporal: O(n) , Complejidad espacial: O(1)
         return math.factorial(n) // (math.factorial(r) * math.factorial(n - r))
 
     @staticmethod
-    def combinaciones_con_repeticion(n, r):
+    def combinaciones_con_repeticion(n, r):    #Complejidad temporal: O(n) , Complejidad espacial: O(1)
         return math.factorial(n + r - 1) // (math.factorial(r) * math.factorial(n - 1))
