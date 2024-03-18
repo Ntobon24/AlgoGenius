df764ac684e5f59ad42f21b6621926c54440c1e0
Matt <116335873+TartarusBoss@users.noreply.github.com>
2024-03-17 23:25:29 -0500

    Update ViewExercise.py

--- Taller_2/View/ViewExercise.py
+++ Taller_2/View/ViewExercise.py
@@ -2,10 +2,12 @@ from Taller_2.Model.Model import Formulas
 
 
 def MenuPrincipal():
+
+    #Complejidad temporal: O(1), espacial: O(1).
     while True:
         print("¿El orden en que se seleccionan los elementos importa? ")
         print("1. Si")
-        print("2. No")
+        print("2. No")A
         print("3. Salir a menu principal")
 
         opcion = input("Seleccione una opción: ")
@@ -21,8 +23,8 @@ def MenuPrincipal():
         else:
             print("Opción no válida. Por favor, seleccione una opción válida.")
 
-
 def menuOrdenImporta():
+    #Complejidad temporal: O(1), espacial: O(1).
     while True:
         print("¿Intervienen todos los elementos? ")
         print("1. Si")
@@ -35,19 +37,17 @@ def menuOrdenImporta():
             print("Ha seleccionado la opción 1")
             OIIntervienenTodos()
             break
-
         elif opcion == "2":
             print("Ha seleccionado la opción 2")
             OINoIntervienenTodos()
             break
-
         elif opcion == "3":
             break
         else:
             print("Opción no válida. Por favor, seleccione una opción válida.")
-
 
 def MenuOrdenNoImporta():
+    #Complejidad temporal: O(1), espacial: O(1).
     while True:
         print("¿Se repiten elementos? ")
         print("1. Si")
@@ -57,28 +57,26 @@ def MenuOrdenNoImporta():
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
-            print("Para la opcion seleccionada se debe realizar una combinacion con repeticion")
+            print("Para la opción seleccionada se debe realizar una combinación con repetición")
             print("Ingresa la cantidad de elementos y de a cuanto se van a agrupar")
             n = int(input("Cantidad de elementos: "))
             r = int(input("Cantidad de elementos a agrupar: "))
-            print(Formulas.combinaciones_con_repeticion(n, r))
+            print(f"El resultado es: {Formulas.combinaciones_con_repeticion(n, r)}")
             break
-
         elif opcion == "2":
-            print("Para la opcion seleccionada se debe realizar una combinacion sin repeticion")
+            print("Para la opción seleccionada se debe realizar una combinación sin repetición")
             print("Ingresa la cantidad de elementos y de a cuanto se van a agrupar")
             n = int(input("Cantidad de elementos: "))
             r = int(input("Cantidad de elementos a agrupar: "))
-            print(Formulas.combinaciones_sin_repeticion(n, r))
+            print(f"El resultado es: {Formulas.combinaciones_sin_repeticion(n, r)}")
             break
-
         elif opcion == "3":
             break
         else:
             print("Opción no válida. Por favor, seleccione una opción válida.")
-
 
 def OIIntervienenTodos():
+    #Complejidad temporal: O(1), espacial: O(1).
     while True:
         print("¿Se repiten elementos? ")
         print("1. Si")
@@ -88,28 +86,26 @@ def OIIntervienenTodos():
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
-            print("Para la opcion seleccionada se debe realizar una permutacion con repeticion")
+            print("Para la opción seleccionada se debe realizar una permutación con repetición")
             print("Ingresa la cantidad de elementos y de a cuanto se van a agrupar")
             n = int(input("Cantidad de elementos: "))
             r = int(input("Cantidad de elementos a agrupar: "))
-            print(Formulas.permutaciones_con_repeticion(n, r))
+            print(f"El resultado es: {Formulas.permutaciones_con_repeticion(n, r)}")
             break
-
         elif opcion == "2":
-            print("Para la opcion seleccionada se debe realizar una permutacion sin repeticion")
+            print("Para la opción seleccionada se debe realizar una permutación sin repetición")
             print("Ingresa la cantidad de elementos y de a cuanto se van a agrupar")
             n = int(input("Cantidad de elementos: "))
             r = int(input("Cantidad de elementos a agrupar: "))
-            print(Formulas.permutaciones_sin_repeticion(n, r))
+            print(f"El resultado es: {Formulas.permutaciones_sin_repeticion(n, r)}")
             break
-
         elif opcion == "3":
             break
         else:
             print("Opción no válida. Por favor, seleccione una opción válida.")
-
 
 def OINoIntervienenTodos():
+    #Complejidad temporal: O(1), espacial: O(1).
     while True:
         print("¿Se repiten elementos? ")
         print("1. Si")
@@ -119,22 +115,22 @@ def OINoIntervienenTodos():
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
-            print("Para la opcion seleccionada se debe realizar una variación con repeticion")
+            print("Para la opción seleccionada se debe realizar una variación con repetición")
             print("Ingresa la cantidad de elementos y de a cuanto se van a agrupar")
             n = int(input("Cantidad de elementos: "))
             r = int(input("Cantidad de elementos a agrupar: "))
-            print(Formulas.variaciones_con_repeticion(n, r))
+            print(f"El resultado es: {Formulas.variaciones_con_repeticion(n, r)}")
             break
-
         elif opcion == "2":
-            print("Para la opcion seleccionada se debe realizar una variación sin repeticion")
+            print("Para la opción seleccionada se debe realizar una variación sin repetición")
             print("Ingresa la cantidad de elementos y de a cuanto se van a agrupar")
             n = int(input("Cantidad de elementos: "))
             r = int(input("Cantidad de elementos a agrupar: "))
-            print(Formulas.variaciones_sin_repeticion(n, r))
+            print(f"El resultado es: {Formulas.variaciones_sin_repeticion(n, r)}")
             break
-
         elif opcion == "3":
             break
         else:
             print("Opción no válida. Por favor, seleccione una opción válida.")
+
+MenuPrincipal()
