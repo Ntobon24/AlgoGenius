1ea3fb074ca56d86e21b62120c5f940fa93bbe2d
santiagog048 <gallo9290@gmail.com>
2024-05-04 15:20:19 -0500

    Heap Sort

--- Taller_3/Model/Algoritmos/HeapSort.py
+++ Taller_3/Model/Algoritmos/HeapSort.py
@@ -12,3 +12,14 @@ def heapify(arr, n, i):
     if mayor != i:
         arr[i], arr[mayor] = arr[mayor], arr[i]
         heapify(arr, n, mayor)
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+    return arr
