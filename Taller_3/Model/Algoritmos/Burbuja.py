0a4dc3191d9f73dc35b2d6800e71b3fbcdbed788
Matt <116335873+TartarusBoss@users.noreply.github.com>
2024-05-04 17:37:58 -0500

    Update Burbuja.py (without API implementation)

--- Taller_3/Model/Algoritmos/Burbuja.py
+++ Taller_3/Model/Algoritmos/Burbuja.py
@@ -1,1 +1,16 @@
+#Algoritmo burbuja sin implementar la API
+def burbuja(columna):
+    n = len(columna)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if columna[j] > columna[j+1]:
+                columna[j], columna[j+1] = columna[j+1], columna[j]
+    return columna
+
+# Ejemplo:
+columna_api = [64, 34, 25, 12, 22, 11, 90]
+print("Algoritmo de ordenamiento: Burbuja")
+print("Columna original:", columna_api)
+print("Columna ordenada:", burbuja(columna_api))
+
 
