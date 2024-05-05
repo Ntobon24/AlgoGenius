69305e10605168453215af05dc0362193ffc0045
Matt <116335873+TartarusBoss@users.noreply.github.com>
2024-05-05 17:46:38 -0500

    Update Seleccion to work with API.py

--- Taller_3/Model/Algoritmos/Seleccion.py
+++ Taller_3/Model/Algoritmos/Seleccion.py
@@ -1,21 +1,8 @@
-#Algoritmo de Seleccion sin implementar la API
-def seleccion(lista):
-    n = len(lista)
+def selection_sort(arr, key):
+    n = len(arr)
     for i in range(n):
-        # Encontrar el índice del mínimo elemento en el resto de la lista
         min_idx = i
         for j in range(i + 1, n):
-            if lista[j] < lista[min_idx]:
+            if arr[j][key] < arr[min_idx][key]:
                 min_idx = j
-        
-        # Intercambiar el mínimo encontrado con el primer elemento no ordenado
-        lista[i], lista[min_idx] = lista[min_idx], lista[i]
-    
-    return lista
-
-# Ejemplo:
-lista = [64, 34, 25, 12, 22, 11, 90]
-print("Algoritmo de ordenamiento: Seleccion")
-print("Lista original:", lista)
-print("Lista ordenada:", seleccion(lista))
-
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
