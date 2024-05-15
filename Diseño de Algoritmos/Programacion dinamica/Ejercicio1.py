96abaf76df8c7f0f9ea01a26160d6b5fbe3fc0a5
Matt <116335873+TartarusBoss@users.noreply.github.com>
2024-05-15 15:48:20 -0500

    Update Ejercicio1.py

--- Diseño de Algoritmos/Programacion dinamica/Ejercicio1.py
+++ Diseño de Algoritmos/Programacion dinamica/Ejercicio1.py
@@ -1,26 +1,12 @@
-# Ejercicio 1
-def factorizacion(n):
-    factores = [0] * (n + 1)
+# Paradigma utilizado -> Programación dinámica
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    dp = [0] * (n+1)
+    dp[0] = 1
+    dp[1] = 1
 
-    for i in range(2, n + 1):
-        if factores[i] == 0:
-            for j in range(i, n + 1, i):
-                if factores[j] == 0:
-                    factores[j] = i
-
-    return factores
-
-
-def factores_primos(n):
-    factores = factorizacion(n)
-    primos = []
-
-    while n > 1:
-        primos.append(factores[n])
-        n //= factores[n]
-
-    return primos
-
-
-num = 15
-print(f"Los factores primos de {num} son {factores_primos(num)}")
+    for i in range(2, n+1):
+        dp[i] = i * dp[i-1]
+    return dp[n]
+print(factorial(5)) 
