d77c5bdc52537bc45b944cd7474cf800041a3035
Matt <116335873+TartarusBoss@users.noreply.github.com>
2024-05-15 15:49:44 -0500

    Update Ejercicio1.py

--- Diseño de Algoritmos/Programacion dinamica/Ejercicio1.py
+++ Diseño de Algoritmos/Programacion dinamica/Ejercicio1.py
@@ -1,12 +1,10 @@
-# Paradigma utilizado -> Programación dinámica
+#Paradigma utilizado -> Programación dinamcia
 def factorial(n):
-    if n == 0 or n == 1:
-        return 1
     dp = [0] * (n+1)
     dp[0] = 1
-    dp[1] = 1
 
-    for i in range(2, n+1):
+    for i in range(1, n+1):
         dp[i] = i * dp[i-1]
     return dp[n]
+
 print(factorial(5)) 
